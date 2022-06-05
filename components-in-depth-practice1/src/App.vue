a279baa852906cce2f262f4f0cd6f1dfe00e3871
Erichy-dev <erichnyaga1@gmail.com>
2022-06-05 18:43:44 +0300

    finished a practice sample of props

---
+++ components-in-depth-practice1/src/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+import MyTechWorld from "./components/myTechWorld.vue";
+import props from "./components/myTechWorld.vue";
+</script>
+
+<template>
+  <MyTechWorld
+    :name="{ firstName: 'Eric', secondName: 'Nyaga' }"
+    current-programming-languages="
+      Python,
+      JavaScript,
+      Typescript,
+      CSS,
+    "
+    favorite-i-d-e="VSCode"
+    level-of-expertise="expert"
+    :technologies="['vue', 'express', 'nodeJs', 'Sequelize', 'SQL', 'Django']"
+    passion
+  />
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+h3 {
+  font-weight: 700;
+}
+#code-body {
+  background-color: rgb(243, 247, 250);
+}
+code {
+  background-color: rgb(243, 243, 183);
+}
+.hint {
+  background-color: rgb(248, 250, 240);
+  font-family: cursive;
+}
+label {
+  color: rgb(173, 110, 110);
+}
+ul.a {
+  list-style-type: georgian;
+}
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+}
+</style>
---
+++ components-in-depth-practice1/src/components/myTechWorld.vue
@@ -0,0 +1,67 @@
+<script setup lang="ts">
+interface pp {
+  name: { firstName?: string; secondName?: string };
+  favoriteIDE: "VSCode" | "Atom";
+  currentProgrammingLanguages?: string;
+  levelOfExpertise?: string;
+  technologies?: string[];
+  passion?: boolean;
+}
+const props = withDefaults(defineProps<pp>(), {
+});
+</script>
+
+<template>
+  <div id="code-body">
+    <h3>TIPS</h3>
+    <ul class="a hint">
+      <li>
+        Type-based-declaration, <code>variable?: type</code> means the variable
+        can be defined or undefined. To declare that it should be always defined
+        use
+        <code>variable: type</code>
+        >
+      </li>
+      <li>
+        Using <code>withDefaults()</code> outputs
+        <label
+          >"Default export of the module has or is using private name"</label
+        >
+        error and will also not work when defining the default values unless you
+        explicitly import the Prop object in the required module.
+      </li>
+      <li>
+        To provide varying data types use<code> age: [String, Number] </code>for
+        <code>defineProps({})</code>. However, for
+        <code> defineProps&#60;{}>()</code> use
+        <code> age: string|number.</code>. Also, for
+        <code>defineProps({variable: [value1, value2]})</code> instead inside
+        <code>instance Props {}</code> use
+        <code>variable: value1|value2</code>
+      </li>
+    </ul>
+    <hr />
+    <h3>MY ROUGH WORK</h3>
+    <ul>
+      <li v-bind="name">
+        My name is {{ name.firstName }} {{ name.secondName }}
+      </li>
+      <li>my favorite IDE is {{ favoriteIDE }}<span></span></li>
+      <li>I've exprience in {{ currentProgrammingLanguages }}</li>
+      <li>I am a programming {{ levelOfExpertise }}</li>
+      <li>
+        List of my web-api technologies.
+        <span class="hint"
+          >Use string[] to describe an array type in the props</span
+        >
+        <ul v-for="technology in technologies" :key="technology">
+          <li>{{ technology }}</li>
+        </ul>
+      </li>
+      <li v-show="passion">
+        I love software development
+        <span class="hint">(Toggling this tag using a boolean)</span>
+      </li>
+    </ul>
+  </div>
+</template>
