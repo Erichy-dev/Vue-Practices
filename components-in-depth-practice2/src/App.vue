0b863d6afc7fbb76864e0fb8c0620ac1e7452f22
Erichy-dev <erichnyaga1@gmail.com>
2022-06-06 05:29:58 +0300

    set up the second practice

---
+++ components-in-depth-practice2/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+</script>
+
+<template>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+@media (min-width: 1024px) {
+  body {
+    display: flex;
+    place-items: center;
+  }
+
+  #app {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    padding: 0 2rem;
+  }
+}
+</style>
