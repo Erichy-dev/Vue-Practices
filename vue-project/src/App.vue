5726885e23c42dd5c1e7cfe755cfcfa1f8f1e68f
Erichy-dev <erichnyaga1@gmail.com>
2022-06-25 15:02:35 +0300

    provide vs defineProps

---
+++ vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { provide, ref } from "vue";
+import Props_vs_Provide from "./components/Props_vs_Provide.vue";
+const myLanguage = ref();
+const myIDE = ref("Visual Studio");
+// Shortcut to all this
+provide("myIDE", myIDE)
+</script>
+
+<template>
+  <div id="parent">
+    What's your favourite Programming Language?<br />
+    <input type="text" id="favLanguage" v-model="myLanguage">
+    <br />
+    What's your favourite IDE?<br />
+    <input type="text" id="favIDE" v-model="myIDE"/>
+    <br />
+    <Props_vs_Provide :programming-lang="myLanguage" />
+  </div>
+</template>
+
+<style>
+button {
+  min-block-size: 3em;
+  min-width: 3em;
+  background-color: chocolate;
+  border-style: inset;
+  border-radius: 3em;
+}
+#parent {
+  color: rgb(39, 44, 183);
+  padding-top: 12em;
+  padding-left: 20em;
+}
+</style>
---
+++ vue-project/src/components/Props_vs_Provide.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { inject, ref } from "vue";
+
+// interface pp {
+//   programmingLang: string;
+//   IDE?: string;
+
+// }
+withDefaults(
+  defineProps<{
+    programmingLang: string;
+    IDE?: string;
+  }>(),
+  {
+    programmingLang: "Typescript",
+  }
+);
+
+// The shortcut
+const myIDE = inject("myIDE")
+</script>
+<template>
+  <main id="child">
+    <button>{{ programmingLang }}</button> &nbsp;
+    <button>{{ myIDE }}</button>
+  </main>
+</template>
+
+<style scoped>
+main{
+  margin: 2em;
+}
+</style>
